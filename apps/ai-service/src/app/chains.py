a5f3ea2c887be300b34602e21d8cc295d8afdd22
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2025-03-31 01:05:08 +0900

    [fix] huggingface llm -> 라마로 수정

--- apps/ai-service/src/app/chains.py
+++ apps/ai-service/src/app/chains.py
@@ -1,16 +1,13 @@
  # RAG 체인 등 LangChain 체인 구성 코드
 from langchain.chains import RetrievalQA
-from langchain_community.llms import HuggingFaceHub
+from langchain_community.llms import Ollama
 from app import config
 
 def create_rag_chain(retriever):
     """
     HuggingFaceHub LLM과 retriever를 이용해 RetrievalQA 체인을 생성
     """
-    llm = HuggingFaceHub(
-        repo_id=config.HUGGINGFACE_REPO_ID,
-        model_kwargs={"temperature": 0.2, "max_length": 256},
-        huggingfacehub_api_token=config.HUGGINGFACEHUB_API_TOKEN
-    )
+    llm = Ollama(model="llama3")
+
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     return chain
