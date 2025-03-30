6084a4d9c50b1314dab609fb0dfa53ff02b00cc3
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2025-03-31 01:12:37 +0900

    [fix] 라마 모델 변경에따른 수정

--- apps/ai-service/src/app/chains.py
+++ apps/ai-service/src/app/chains.py
@@ -1,13 +1,14 @@
  # RAG 체인 등 LangChain 체인 구성 코드
 from langchain.chains import RetrievalQA
-from langchain_community.llms import Ollama
+#from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 from app import config
 
 def create_rag_chain(retriever):
     """
     HuggingFaceHub LLM과 retriever를 이용해 RetrievalQA 체인을 생성
     """
-    llm = Ollama(model="llama3")
+    llm = OllamaLLM(model="llama3")
 
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     return chain
