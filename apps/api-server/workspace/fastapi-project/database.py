2b6f718f4b437af9297860cea8146b4f93c39cda
kim_d0_2 <166965563+dokkang01@users.noreply.github.com>
2025-05-19 23:05:45 +0900

    DB 버그 수정2

--- apps/api-server/workspace/fastapi-project/database.py
+++ apps/api-server/workspace/fastapi-project/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234asdf@localhost/fastapi_project"
+load_dotenv()  # .env 파일을 불러와서 환경변수로 등록
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
