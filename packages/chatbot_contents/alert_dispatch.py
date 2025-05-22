bd802ccf8321a36649088775491105c4adea36b6
ssshinnpson <ddae0000@gmail.com>
2025-05-23 06:39:57 +0900

    [Update] alert_dispatch

--- packages/chatbot_contents/alert_dispatch.py
+++ packages/chatbot_contents/alert_dispatch.py
@@ -1,60 +1,61 @@
 from pydantic import BaseModel, Field
 from typing import Optional, Dict, Any, Literal
 from typing_extensions import Literal
+from datetime import date
 
-class AlertDispatch(BaseModel):
-    intent: Literal["ALERT_DISPATCH"]= Field(..., exclude=True)
-    eventType: Literal["price_drop","wx_risk","cancel_deadline"]
-    channel: Literal["email","kakao"]
-    userId: Optional[str]
-    payload: dict
-
-class ContentsList(BaseModel):
-    message: str
-    contents : AlertDispatch
+class PayloadList(BaseModel):
+    origin: str
+    dest: str
+    departure_date: date
+    selling_price: int
+    price_threshold: int
+    deadline: Optional[date]
 
 
 class AlertDispatchContent(BaseModel):
     intent: Literal["ALERT_DISPATCH"]= Field(..., exclude=True)
-    contents: ContentsList
-
-
-
-    def validate_payload(self) -> bool:
-      required_keys = {"route", "oldPrice", "newPrice", "currency", "dropPercent"}
-      return required_keys.issubset(self.contents.payload.keys())
-
-{
-  "intent": "ALERT_DISPATCH",
-  "contents": {
-    "userId": "user-1234",
-    "eventId": "1",
-    "message": "알림을 받으실 이메일을 입력해주세요.",
-    "eventType": "price_drop",
-    "channel": "email",
-    "payload": {
-      "route": "ICN→LAX",
-      "oldPrice": 1300000,
-      "newPrice": 1200000,
-      "currency": "KRW",
-      "dropPercent": 7.7
-    }
-  }
-}
-
-
-
-class PriceDropPayload(BaseModel):
-    route: str               # ex. "ICN→LAX"
-    oldPrice: float          # 기존 가격
-    newPrice: float          # 현재(혹은 목표) 가격
-    currency: str            # ex. "KRW"
-    dropPercent: float       # ex. 7.7
-
-class AlertDispatchPriceDrop(BaseModel):
-    intent: Literal["ALERT_DISPATCH_PRICE_DROP"] = Field(..., exclude=True)
-    channel: Literal["email", "kakao"]
-    userId: str
+    eventType: Literal["price_drop","wx_risk","cancel_deadline"]
+    channel: Literal["email","kakao"]
     message: str
-    payload: PriceDropPayload
+    userId: str
+    emailId: str
+    payload: PayloadList
+
+
+
+if __name__ == "__main__":
+    raw = {"intent": "ALERT_DISPATCH",
+           "eventType": "price_drop",
+           "channel": "email",
+           "userId": "user-1234",
+           "emailId": "hello@gmail.com",
+           "message": "1300000원 이하로 떨어지면 알림을 받으시길 원하시나요?",
+           "payload": {
+              "origin": "서울",
+              "dest": "뉴욕",
+              "departure_date": "2025-06-25",
+              "selling_price": 15000000,
+              "price_threshold": 1300000,
+              "deadline": "2025-06-18"
+            }
+    }
+    
+    content = AlertDispatchContent.model_validate(raw)
+    print(content.payload)  
+
+
+
+# class PriceDropPayload(BaseModel):
+#     route: str               # ex. "ICN→LAX"
+#     oldPrice: float          # 기존 가격
+#     newPrice: float          # 현재(혹은 목표) 가격
+#     currency: str            # ex. "KRW"
+#     dropPercent: float       # ex. 7.7
+
+# class AlertDispatchPriceDrop(BaseModel):
+#     intent: Literal["ALERT_DISPATCH_PRICE_DROP"] = Field(..., exclude=True)
+#     channel: Literal["email", "kakao"]
+#     userId: str
+#     message: str
+#     payload: PriceDropPayload
 
