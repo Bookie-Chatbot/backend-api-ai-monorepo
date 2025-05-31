b94d670b913b6cddfef8fbfa70bce04c2f1111fb
kim_d0_2 <166965563+dokkang01@users.noreply.github.com>
2025-05-31 11:38:03 +0900

    이메일 까지 보내짐 ㅠㅠ

--- apps/api_server/workspace/fastapi_project/utils/email_alert.py
+++ apps/api_server/workspace/fastapi_project/utils/email_alert.py
@@ -4,8 +4,8 @@ from email.mime.text import MIMEText
 def send_email(to_email: str, subject: str, body: str):
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    sender_email = "loi97weewee@gmail.com"
-    sender_password = "Dontforget"  # Gmail은 앱 비밀번호 필요
+    sender_email = "bookie.tripplanner@gmail.com"
+    sender_password = "dvqt uxvf ihhy feva"  # Gmail은 앱 비밀번호 필요
 
     msg = MIMEText(body)
     msg["Subject"] = subject
