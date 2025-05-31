3b2c1b7c06768e374299a1890aa8891712a3e520
kim_d0_2 <166965563+dokkang01@users.noreply.github.com>
2025-05-31 12:00:32 +0900

    gmail 비밀번호 dotenv에 저장

--- apps/api_server/workspace/fastapi_project/utils/email_alert.py
+++ apps/api_server/workspace/fastapi_project/utils/email_alert.py
@@ -1,11 +1,15 @@
 import smtplib
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def send_email(to_email: str, subject: str, body: str):
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
     sender_email = "bookie.tripplanner@gmail.com"
-    sender_password = "dvqt uxvf ihhy feva"  # Gmail은 앱 비밀번호 필요
+    sender_password = os.getenv("SENDER_PW")  # Gmail은 앱 비밀번호 필요
 
     msg = MIMEText(body)
     msg["Subject"] = subject
